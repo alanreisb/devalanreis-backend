550ef8c577f12fffc468939a948f399fde14cde3
Alan Reis Belarmino <alanreisb@gmail.com>
2019-06-17 18:39:22 -0300

    Mudança de porta heroku

--- src/server.js
+++ src/server.js
@@ -50,5 +50,5 @@ app.post("/contatos/enviar", urlencodedParser, function (req, res) {
     console.log(message);
 });
 
-app.listen(3333, ()=> {console.log('Servidor iniciado no local 3333...')});
+app.listen(process.env.PORT||3333, ()=> {console.log('Servidor iniciado no local 3333...')});
 
