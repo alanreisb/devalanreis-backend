f08c120728cdb01495b9c0d72964db27afab9a3a
Alan Reis - Desktop <alanreisb@gmail.com>
2020-04-17 19:55:53 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,53 +1,46 @@
-require('dotenv').config();
-const express = require("express");
-const cors = require('cors');
-const app = express();
-const mailer = require("nodemailer");
-const bodyParser = require('body-parser');
+require('dotenv').config()
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const mailer = require('nodemailer')
+const bodyParser = require('body-parser')
 
-var urlencodedParser = bodyParser.urlencoded({ extended: false });
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 const config = {
+  host: 'smtp.mail.yahoo.com',
+  port: 465,
+  service: 'Yahoo',
 
-    host: 'smtp.mail.yahoo.com',
-    port: 465,
-    service: 'Yahoo',
+  auth: {
+    user: process.env.EMAIL,
+    pass: process.env.PASSWORD
+  },
+  debug: false,
+  logger: true,
+  tls: {
+    rejectUnauthorized: false
+  }
+}
 
-    auth: {
+const transporter = mailer.createTransport(config)
 
-        user: process.env.EMAIL,
-        pass: process.env.PASSWORD
-    },
-    debug: false,
-    logger: true,
-    tls: {
-        rejectUnauthorized: false
+app.use(bodyParser.json())
+
+app.post('/contatos/enviar', urlencodedParser, function (req, res) {
+  const message = {
+    from: process.env.EMAIL,
+    to: process.env.PARA,
+    subject: 'Mensagem Portfolio: ' + req.body.nome + '---' + req.body.email,
+    text: req.body.mensagem
+  }
+  transporter.sendMail(message, (error, info) => {
+    if (error) {
+      return res.status(400).send(`${error}  ${info}`)
     }
+    return res.status(200).send('Enviou')
+  })
+  console.log(message)
+})
 
-};
-
-const transporter = mailer.createTransport(config);
-
-app.use(bodyParser.json());
-app.use(cors({origin:`${process.env.APP_CORS}`}));
-
-
-
-app.post("/contatos/enviar", urlencodedParser, function (req, res) {
-    const message = {
-        from: process.env.EMAIL,
-        to: process.env.PARA,
-        subject: "Mensagem Portfolio: " +req.body.nome + "---" +req.body.email ,
-        text: req.body.mensagem,
-    };
-    transporter.sendMail(message, (error, info) => {
-        if (error) {
-            return res.status(400).send(`${error}  ${info}`);
-        }
-        return res.status(200).send("Enviou");
-    });
-    console.log(message);
-});
-
-app.listen(process.env.PORT||3333);
-
+app.listen(process.env.PORT || 3333)
